d5c0afd970bee9b015eb66b0e691cc4a5a096a15
Brendan <brendanewen@gmail.com>
2020-06-05 17:27:15 +0100

    new file chnages
    made new file and worked on navbar redesign there. just need to sort out dropdown menu and make the page fixed width after 1200px

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 // window.addEventListener("hashchange", function() { scrollBy(0, -75, ) })
 function changeClass(){
   var el = document.getElementById("main_ul");
+  var ban = document.getElementById("banner");
+  var nav = document.getElementById('main_nav');
+  var menuBtn = document.getElementById('menuImg');
   if(el.classList.contains("show")){
     el.classList.remove("show");
     el.classList.add("hide");
+
+    nav.classList.remove("height_inc");
+    menuBtn.src = "https://cdn1.iconfinder.com/data/icons/starter-mobile-app/100/mobile_button-16-512.png";
+
+    ban.classList.add("show");
+    ban.classList.remove("hide");
   }else{
     el.classList.add("show");
     el.classList.remove("hide");
+
+    nav.classList.add("height_inc");
+    menuBtn.src = "https://cdn4.iconfinder.com/data/icons/web-app-ui/64/web-navigation-ui_icon-49-512.png";
+
+    ban.classList.remove("show");
+    ban.classList.add("hide");
   };
   console.log("test");
+};
+
+function dropdown(){
+  var ell = document.getElementById("scnd_ul");
+
+  if(ell.classList.contains("show")){
+    ell.classList.remove("show");
+    ell.classList.add("hide");
+  }else{
+    ell.classList.add("show");
+    ell.classList.remove("hide");
+  };
 };
